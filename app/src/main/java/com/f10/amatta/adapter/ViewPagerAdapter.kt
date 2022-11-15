e63e81ba45dcbbd1de0e55a434506c7f395c73c7
KOREAparksh <tmdgks2222@naver.com>
2022-11-15 17:37:47 +0900

    Feat: viewpager test option 설정

---
+++ app/src/main/java/com/f10/amatta/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,25 @@
+package com.f10.amatta.adapter
+
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.f10.amatta.R
+
+class ViewPagerAdapter(var items: ArrayList<String> = arrayListOf()) : RecyclerView.Adapter<ViewPagerAdapter.ViewHolder>() {
+
+
+    inner class ViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder
+        (LayoutInflater.from(parent.context).inflate(R.layout.viewpager_item, parent, false)) {
+        val aespaMember = itemView.findViewById<TextView>(R.id.test_text)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = this.ViewHolder((parent))
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+       holder.aespaMember.setText("123")
+    }
+
+    override fun getItemCount(): Int = items.size
+
+}
--- app/src/main/java/com/f10/amatta/view/MainActivity.kt
+++ app/src/main/java/com/f10/amatta/view/MainActivity.kt
@@ -2,12 +2,22 @@ package com.f10.amatta.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 import com.f10.amatta.R
+import com.f10.amatta.adapter.ViewPagerAdapter
+import com.f10.amatta.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var cardList: ArrayList<String> = arrayListOf("123", "222", "333")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root)
 
+        binding.cardList.adapter = ViewPagerAdapter(this.cardList)
+        binding.cardList.orientation = ViewPager2.ORIENTATION_HORIZONTAL
     }
 }
