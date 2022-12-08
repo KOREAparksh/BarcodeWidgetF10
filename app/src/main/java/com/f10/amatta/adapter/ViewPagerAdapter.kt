0cb53cdeb45d03995d214a741566f3de1c69ac34
KOREAparksh <tmdgks2222@naver.com>
2022-12-08 22:24:03 +0900

    Feat: viewpager 이미지를 제외한 레이아웃 구현

--- app/src/main/java/com/f10/amatta/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/f10/amatta/adapter/ViewPagerAdapter.kt
@@ -7,19 +7,20 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.f10.amatta.R
 
-class ViewPagerAdapter(var items: ArrayList<String> = arrayListOf()) : RecyclerView.Adapter<ViewPagerAdapter.ViewHolder>() {
-
-
-    inner class ViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder
-        (LayoutInflater.from(parent.context).inflate(R.layout.viewpager_item, parent, false)) {
-        val aespaMember = itemView.findViewById<TextView>(R.id.test_text)
-    }
+class ViewPagerAdapter(var items: ArrayList<String> = arrayListOf())
+                : RecyclerView.Adapter<ViewPagerAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = this.ViewHolder((parent))
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-       holder.aespaMember.setText("123")
+       holder.aespaMember.setText(items[position])
     }
 
     override fun getItemCount(): Int = items.size
+
+    inner class ViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder
+        (LayoutInflater.from(parent.context).inflate(R.layout.viewpager_item, parent, false)) {
+        val aespaMember = itemView.findViewById<TextView>(R.id.name)
+    }
 
 }
--- app/src/main/java/com/f10/amatta/view/MainActivity.kt
+++ app/src/main/java/com/f10/amatta/view/MainActivity.kt
@@ -2,10 +2,13 @@ package com.f10.amatta.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.viewpager2.widget.ViewPager2
 import com.f10.amatta.R
 import com.f10.amatta.adapter.ViewPagerAdapter
 import com.f10.amatta.databinding.ActivityMainBinding
+import com.f10.amatta.decoration.HorizontalMarginItemDecoration
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,8 +19,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         this.binding = ActivityMainBinding.inflate(layoutInflater);
         setContentView(binding.root)
+        setViewPager();
+    }
 
+    private fun setViewPager(){
+        linkAdapter()
+        setViewPagerPreview()
+    }
+
+    private fun linkAdapter() {
         binding.cardList.adapter = ViewPagerAdapter(this.cardList)
-        binding.cardList.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        binding.cardList.apply { offscreenPageLimit = 3 }
+    }
+
+    private fun setViewPagerPreview() {
+        val currentItemHorizontalMarginPx = resources
+            .getDimension(R.dimen.viewpager_current_item_horizontal_margin)
+        val nextItemVisiblePx = resources.getDimension(R.dimen.viewpager_next_item_visible)
+        val pageTranslationX = nextItemVisiblePx + currentItemHorizontalMarginPx
+        val pageTransformer = ViewPager2.PageTransformer { page: View, position: Float ->
+            page.translationX = -pageTranslationX * position
+            page.scaleY = 1 - (0.25f * abs(position))
+            page.alpha = 0.25f + (1 - abs(position))
+        }
+        binding.cardList.setPageTransformer(pageTransformer)
+        val itemDecoration = HorizontalMarginItemDecoration(
+            binding.root.context,
+            R.dimen.viewpager_current_item_horizontal_margin
+        )
+        binding.cardList.addItemDecoration(itemDecoration)
     }
 }
